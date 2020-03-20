c61c8d7c69220b9ccb764d66712ada205dee98c0
Michelle Perkins <perkinsinc2016@gmail.com>
2020-03-19 22:21:13 -0500

    java_assesment

---
+++ Module-1_Assessment/activity-4.js
@@ -0,0 +1,14 @@
+//Create an array with 3 student names
+
+const classmate=["Troy","Michelle","Kiyah"];
+
+// in a loop prompt the user for 3 more names
+const classmate= prompt('enter the name of another classmate');
+classmatelist = '';
+
+for (i = 3; i < classmate.length; i++) {
+    
+}
+
+//After every user input store the new name into the array
+classmatlist.push(classmate)
